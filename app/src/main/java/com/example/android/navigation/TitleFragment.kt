bc54d521a9aa46bbe73faa3be6088ed7a588d508
marcoacsena <marcoacsena@gmail.com>
2021-07-16 17:37:08 -0300

    Added a navigation graph to the project: in this task, I modifed my code to replace the TitleFragment with the NavHostFragment, In the activity_main.xml file. Moreover,I  added the title Fragment and the game Fragment to the app's navigation graph. I connected the fragments to each other. Then I added a click handler to the Play button. For this, I modified TitleFragment.kt file.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,8 +21,6 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +29,10 @@ class TitleFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
                         R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_titleFragment_to_gameFragment2)
+        }
 
         return binding.root
     }
